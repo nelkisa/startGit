7567fe15ccecd253696887d32cc94458d947cf40
Nelkisa Matias <nelkisa.matias@mv.com.br>
2018-10-23 14:35:42 -0200

    adcionando os métodos para o projeto da calculadora

---
+++ calculadora/src/calculadora/methods.java
@@ -0,0 +1,25 @@
+package calculadora;
+
+public class methods {
+	
+	public double Somar(double n1, double n2){
+		
+		return(n1+n2);
+	}
+	
+	public double Subtrair(double n1, double n2){
+		
+		return(n1-n2);
+	}
+
+	public double Dividir(double n1, double n2){
+		
+		return(n1/n2);
+	}
+
+	public double Multiplicar(double n1, double n2){
+		
+		return(n1*n2);
+	}
+
+}
